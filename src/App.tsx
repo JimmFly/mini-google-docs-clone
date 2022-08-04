3938862f7b7f955863f74e4348ede9bfb51e7c5b
Fly Gor <447268514@qq.com>
2022-08-04 11:56:25 +0800

    ts练习

--- src/App.tsx
+++ src/App.tsx
@@ -1,78 +1,41 @@
-// Import React dependencies.
-import React, { useState,useCallback } from 'react'
-// Import the Slate editor factory.
-import { createEditor } from 'slate'
-// TypeScript users only add this code
-import { BaseEditor,Editor, Transforms  } from 'slate'
-import { ReactEditor } from 'slate-react'
-import { Slate, Editable, withReact } from 'slate-react'
 
-type CustomElement = { type: 'paragraph'; children: CustomText[] }
-type CustomText = { text: string }
+import React from 'react'
 
-declare module 'slate' {
-  interface CustomTypes {
-    Editor: BaseEditor & ReactEditor
-    Element: CustomElement
-    Text: CustomText
+
+
+
+
+
+function App() {
+
+interface User{
+  admin:boolean
+}
+interface DB {
+  filterUsers(filter:(this:User)=>boolean):User[]
+}
+
+const db:DB = {
+  filterUsers:(filter:(this:User)=>boolean)=>{
+    let user1 ={
+      admin:true
+    }
+    let user2 ={
+      admin:false
+    }
+    return [user1,user2]
   }
 }
-
-
-// Add the initial value.
-const initialValue  = [
-  {
-    type: 'paragraph',
-    children: [{ text: 'A line of text in a paragraph.' }],
-  },
-] as any
-
-const CodeElement = props => {
-  return (
-    <pre {...props.attributes}>
-      <code>{props.children}</code>
-    </pre>
-  )
-}
-
-const DefaultElement = props => {
-  return <p {...props.attributes}>{props.children}</p>
-}
-
-function App () {
-  const [editor] = useState(() => withReact(createEditor()))
-  // Define a rendering function based on the element passed to `props`. We use
-  // `useCallback` here to memoize the function for subsequent renders.
-  const renderElement = useCallback(props => {
-    switch (props.element.type) {
-      case 'code':
-        return <CodeElement {...props} />
-      default:
-        return <DefaultElement {...props} />
-    }
-  }, [])
+const admins=db.filterUsers(function(this:User){
+  return this.admin
+})
+console.log(admins);
 
   return (
-<Slate editor={editor} value={initialValue}>
-      <Editable
-        renderElement={renderElement}
-        onKeyDown={event => {
-          if (event.key === '`' && event.ctrlKey) {
-            event.preventDefault()
-            // Determine whether any of the currently selected blocks are code blocks.
-            const [match] = Editor.nodes(editor, {
-              match: n => n.type === 'code',
-            })
-            // Toggle the block type depending on whether there's already a match.
-            Transforms.setNodes(
-              editor,
-              { type: match ? 'paragraph' : 'code' },
-              { match: n => Editor.isBlock(editor, n) }
-            )
-          }
-        }}
-      />
-    </Slate>
+  <div>
+    this is it 
+    
+  </div>
   )
 }
 
